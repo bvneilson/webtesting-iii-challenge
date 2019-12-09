fd142f29acf03cdf293ead768057a16bafc17727
Neilson <Brennan.Neilson@henryschein.com>
2019-12-09 14:52:38 -0700

    All MVP tests written and passing

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -6,32 +6,13 @@ import '@testing-library/jest-dom/extend-expect';
 
 import Dashboard from './Dashboard.js';
 
-test('Dashboard renders correctly', () => {
+test('Dashboard shows the controls and display', () => {
   const dashboard = render(<Dashboard />);
   expect(dashboard).toMatchSnapshot();
 })
 
-test("gate can't be opened if locked", () => {
-  const dashboard = render(<Dashboard locked={true} hasKeyCard={false} />);
-  const openButton = dashboard.getByText(/open/i);
-  expect(openButton).toBeDisabled();
-  //expect(openButton).not.toBeEnabled();
-})
-
-test("defaults to locked and does not have key card", () => {
+test("defaults to unlocked and open", () => {
   const dashboard = render(<Dashboard />);
-  expect(dashboard.getByText(/locked/i));
-  expect(dashboard.getByText(/insert key card/i));
-})
-
-test("if locked is false and has key card", () => {
-  const dashboard = render(<Dashboard locked={false} hasKeyCard={true} />);
   expect(dashboard.getByText(/unlocked/i));
-  expect(dashboard.getByText(/remove key card/i));
-})
-
-test("button contains locked className when locked", () => {
-  const dashboard = render(<Dashboard locked={true} />);
-  const button = dashboard.getByText(/locked/i);
-  expect(button.classList.contains('locked').toBe(true));
+  expect(dashboard.getByText(/open/i));
 })
